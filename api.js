92bcf066d8d0975380828c8d72b3a619592948f7
savarniknandinee <80510308+savarniknandinee@users.noreply.github.com>
2024-07-23 19:09:40 +0530

    Update #@

--- api.js
+++ api.js
@@ -5,15 +5,18 @@ const API = axios.create({
   baseURL: "https://emkc.org/api/v2/piston",
 });
 
-export const executeCode = async (language, sourceCode) => {
-  const response = await API.post("/execute", {
+export const executeCode = async (language, editorRef) => {
+  const editorValue = editorRef.current.getValue();
+  const requestData = {
     language: language,
     version: languageOptions[language],
     files: [
       {
-        content: sourceCode,
+        content: editorValue,
       },
     ],
-  });
+  };
+
+  const response = await API.post("/execute", requestData);
   return response.data;
 };
--- app/components/LanguageOptions.tsx
+++ app/components/LanguageOptions.tsx
@@ -26,13 +26,14 @@ const LanguageOptions: React.FC<LanguageOptionsProps> = ({ onSelectLanguage }) =
 
   return (
     <div className="flex items-center mr-8 px-6 py-5 space-x-4">
-      <Select
-        options={formatOptions}
-        placeholder="javascript (18.15.0)"
-        className="w-[36em] text-gray-500 text-[14px] rounded-md p-2 focus:outline-none focus:border-green-500"
-        onChange={handleLanguageChange}
-      />
-    </div>
+    <Select
+      instanceId="language-select"
+      options={formatOptions}
+      placeholder="javascript (18.15.0)"
+      className="w-[36em] text-gray-500 text-[14px] rounded-md p-2 focus:outline-none focus:border-green-500"
+      onChange={handleLanguageChange}
+    />
+  </div>
   );
 };
 
